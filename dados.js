654736fcb6fda23ae145b8b6a17bf67ced7676b8
Larissa-mm <martinslarissamoreira@gmail.com>
2024-09-09 14:33:57 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+   
+   {titulo:"Rebeca andrade",
+    descricao:" Rebeca Andrade, a joia da ginástica brasileira, conquistou o coração dos brasileiros com sua determinação e talento.Bicampeã olímpica, ela é a maior medalhista olímpica da história do Brasil, acumulando diversas medalhas em competições nacionais e internacionais" ,
+    link:" https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags:"ouro ginastica rebecca ginasta brasileira "
+   },
+   {
+    titulo: "Beatriz Ferreira",
+    descricao: "Beatriz Ferreira, a 'Menina de Ouro' do boxe brasileiro, conquistou o respeito de todos com suas lutas emocionantes e técnica impecável. Medalhista olímpica e mundial, ela é um dos maiores nomes do boxe feminino.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Ferreira",
+    tags:"boxe  boxeadora bia boxe feminino "
+},
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza, a judoca brasileira, é conhecida por sua força e determinação dentro do tatame. Com diversas medalhas em competições nacionais e internacionais, ela é uma das principais promessas do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags:"judô judo judoka judoca  ouro"
+}
+];
+
+   
